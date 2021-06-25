d38a90c245055a6d50c89d50fa7201f5f5ca78e9
Maryann878 <85343201+Maryann878@users.noreply.github.com>
2021-06-25 11:46:12 +0100

    Add files via upload

--- week4/script.js
+++ week4/script.js
@@ -1,9 +1,69 @@
 
-    function myFunction(){
-        document.getElementById("demo").innerHTML = "Paragraph changed"
-    }
+    // function myFunction(){
+        // document.getElementById("demo").innerHTML = "Paragraph changed"
+    // }
 
 
-    document.write("MaryAnn");
+    // document.write("MaryAnn");
     // window.alert("Frontend");
-    console.log(900);
+   
+    // var x=5;
+    // var y=6;
+    // var y =10;
+    // var z= x + y
+
+    // var y=10;
+    // console.log(z);
+
+    var person = 'maya';
+    var rightWrong = true;
+    var numChildren = undefined;
+    var age= 30;
+    var size= null
+    
+
+    console.log(person)
+    console.log(rightWrong);
+    console.log(numChildren);
+    console.log(age)
+    console.log(size)
+
+
+
+    document.getElementById("person").innerHTML = "person";
+    document.getElementById("boolean").innerHTML = "rightWrong";
+        document.getElementById("number").innerHTML = "numChildren";
+        document.getElementById("age").innerHTML = "age";
+    document.getElementById("null").innerHTML = "size";
+
+    var number1 = 10;
+    var number1 = 20;
+
+    var sum = number1 + number2;
+
+    var a = 5
+    var b = 5
+    var multiple = a + b
+
+    console.log(multiple);
+
+    var h = 20;
+    vari = 10;
+    var subt = h-i;
+    console.log(subt);
+
+    var m = 65;
+    var n =5;
+    var division = m/n
+    console.log(division)
+
+
+    // increment operators
+    var s = 10;
+    s++;
+    console.log("increment operator", s)
+
+    // decrement operators
+    var t = 50;
+    t--;
+    console.log("decrement operator", t);
